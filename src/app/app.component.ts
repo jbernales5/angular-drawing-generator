2576cec9958e34e6c07c71e9c9fee9d57cb32542
Jonathan Bernales <me@jonathan-bernales.com>
2021-01-06 22:11:54 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ import { switchMap, takeUntil, pairwise } from 'rxjs/operators';
 export class AppComponent {
   @ViewChild('canvas') public canvas: ElementRef;
 
-  @Input() public width = 400;
-  @Input() public height = 400;
+  @Input() public width = 200;
+  @Input() public height = 200;
   private cx: CanvasRenderingContext2D;
   private canvasEl: HTMLCanvasElement;
 
@@ -33,7 +33,7 @@ export class AppComponent {
     this.cx.lineWidth = 3;
     this.cx.lineCap = 'round';
     this.cx.strokeStyle = '#000';
-    
+
     // we'll implement this method to start capturing mouse events
     this.captureEvents(this.canvasEl);
   }
@@ -54,23 +54,23 @@ export class AppComponent {
               // pairwise lets us get the previous value to draw a line from
               // the previous point to the current point
               pairwise()
-            )
+            );
         })
       )
       .subscribe((res: [MouseEvent, MouseEvent]) => {
         const rect = canvasEl.getBoundingClientRect();
-  
+
         // previous and current position with the offset
         const prevPos = {
           x: res[0].clientX - rect.left,
           y: res[0].clientY - rect.top
         };
-  
+
         const currentPos = {
           x: res[1].clientX - rect.left,
           y: res[1].clientY - rect.top
         };
-  
+
         // this method we'll implement soon to do the actual drawing
         this.drawOnCanvas(prevPos, currentPos);
       });
@@ -82,25 +82,24 @@ export class AppComponent {
   ) {
     // incase the context is not set
     if (!this.cx) { return; }
-  
+
     // start our drawing path
     this.cx.beginPath();
-  
+
     // we're drawing lines so we need a previous position
     if (prevPos) {
       // sets the start point
       this.cx.moveTo(prevPos.x, prevPos.y); // from
-  
+
       // draws a line from the start pos until the current position
       this.cx.lineTo(currentPos.x, currentPos.y);
-  
+
       // strokes the current path with the styles we set earlier
       this.cx.stroke();
     }
   }
 
   clearContent(){
-    console.log("wololo")
     this.cx.clearRect(0, 0, 400, 400);
   }
 
